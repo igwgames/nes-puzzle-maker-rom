3687586b82d9dca5f8dbe633c45f415101aa68c3
cppchriscpp <admin@cpprograms.net>
2019-01-01 03:56:36 -0500

    version fix

--- source/configuration/game_info.c
+++ source/configuration/game_info.c
@@ -1,3 +1,3 @@
 // A bunch of information about your game - used on the title screen, as well as 
 // the credits, and anywhere else you fancy!
-const unsigned char romVersion[] = "v1.0";
+const unsigned char romVersion[] = "v1.1";
