d4dc958349067843d63e1f4246d6850fa5491a3e
cppchriscpp <admin@cpprograms.net>
2024-08-11 14:21:20 -0400

    Remove old unused sfx

--- source/c/configuration/system_constants.h
+++ source/c/configuration/system_constants.h
@@ -21,15 +21,12 @@
 // Constants for sound effects we play in the game
 #define SFX_HURT 0
 #define SFX_HEART 1
-#define SFX_KEY 2
-#define SFX_GAMEOVER 3
-#define SFX_WIN 4
-#define SFX_PLACE_ITEM 5
-#define SFX_MENU_BOP 6
-#define SFX_MENU_OPEN 7
-#define SFX_MENU_CLOSE 8
-#define SFX_CRATE_SMASH 9
-#define SFX_CRATE_MOVE 10
+#define SFX_WIN 2
+#define SFX_MENU_BOP 3
+#define SFX_MENU_OPEN 4
+#define SFX_MENU_CLOSE 5
+#define SFX_CRATE_SMASH 6
+#define SFX_CRATE_MOVE 7
 
 // Constants for sfx channels. Completely unnecessary but it's easier to read
 #define SFX_CHANNEL_1 0
